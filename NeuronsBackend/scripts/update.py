14136ea0201945712a9f48299dce58f54e4ecb0a
Sohan Naidu <sohannaidu123@gmail.com>
2022-09-04 14:17:59 +0530

    refactor code and updated gitignore

--- NeuronsBackend/scripts/update.py
+++ NeuronsBackend/scripts/update.py
@@ -2,19 +2,34 @@ import sys
 import h5py
 import numpy as np
 
-def modify(upd, index):
+def modify(index, flag):
+    old = np.array(hf[b[index]])
+    oldbias = np.array(hf[b[index - 1]])
+    if(flag):
+        new = np.random.normal(-0.3, 0.3, size = (old.shape[0]))
+        upd = np.insert(old, y, new, axis = 1)
+        newbias = np.zeros(1)
+        updbias = np.insert(oldbias, y, newbias, axis = 0)
+    else:
+        upd = np.delete(old, y, axis = 1)
+        updbias = np.delete(oldbias, y, axis = 0)
     #ith kernel
     del hf[b[index]]
     hf.create_dataset(b[index], (upd.shape[0], upd.shape[1]), data = upd)
     #ith bias
     del hf[b[index - 1]]
-    hf.create_dataset(b[index - 1], (upd.shape[1],))
+    hf.create_dataset(b[index - 1], (upd.shape[1],), data = updbias)
     #(i + 1)th kernel
-    index = 2*(x + 1) + 1
-    if(index < len(b)/2):
-        prev = hf[b[index]].shape[1]
-        del hf[b[index]]
-        hf.create_dataset(b[index], (upd.shape[1], prev))
+    if(index + 2 < len(b)):
+        oldnextkernel = np.array(hf[b[index + 2]])
+        prev = hf[b[index + 2]].shape[1]
+        if(flag):
+            newnextkernel = np.zeros(shape = (1, prev))
+            updnextkernel = np.insert(oldnextkernel, y, newnextkernel, axis = 0)
+        else:
+            updnextkernel = np.delete(oldnextkernel, y, axis = 0)
+        del hf[b[index + 2]]
+        hf.create_dataset(b[index + 2], (upd.shape[1], prev), data = updnextkernel)
 
 [_, op, x, y] = sys.argv
 x = int(x)
@@ -28,26 +43,24 @@ b = [groups[i] for i in range(len(groups)) if "model" in groups[i] and ("bias" i
 index = 2*x + 1
 if(index < 0 or index >= len(b)):
     raise IndexError("Invalid layer.")
-old = np.array(hf[b[index]])
+if(index == len(b) - 1):
+    raise IndexError("Cannot modify output layer.")
 
 if(op == "add"):
-    new = np.random.normal(-0.3, 0.3, size = (old.shape[0]))
     try:
-        upd = np.insert(old, y, new, axis = 1)
-        modify(upd, index)
+        modify(index, 1)
         hf.close()
         print("Successfully added a neuron to layer " + str(x) + " at position " + str(y) + ".")
     except IndexError:
-        print("Invalid position.")
+        print("Invalid layer.")
 
 elif(op == "del"):
     try:
-        upd = np.delete(old, y, axis = 1)
-        modify(upd, index)
+        modify(index, 0)
         hf.close()
         print("Successfully deleted a neuron in layer " + str(x) + " at position " + str(y) + ".")
-    except IndexError:
-        print("Invalid position.")
+    except Exception as e:
+        print("Invalid position." + str(e))
 
 else:
     raise Exception("Invalid operator.")
